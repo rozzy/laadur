306388471196c07c4e28e5a10d4653d768c6c4ed
Nikita Nikitin <berozzy@gmail.com>
2013-10-25 23:03:17 +0300

    OptionsParser

--- lib/laadur.rb
+++ lib/laadur.rb
@@ -1,8 +1,12 @@
 require "laadur/version"
-
+require 'optparse'
+require 'pp'
 module Laadur
   class CLI
     def initialize
+      puts 'test'
+      options = {}
+
       optparse = OptionParser.new do|opts|
         # TODO: Put command-line options here
         
@@ -13,6 +17,17 @@ module Laadur
           exit
         end
       end
+
+      # Parse the command-line. Remember there are two forms
+      # of the parse method. The 'parse' method simply parses
+      # ARGV, while the 'parse!' method parses ARGV and removes
+      # any options found there, as well as any parameters for
+      # the options. What's left is the list of files to resize.
+      optparse.parse!
+
+      pp "Options:", options
+      pp "ARGV:", ARGV
+   
     end
   end
 end
