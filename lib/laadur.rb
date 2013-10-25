2e880a32eef59326804c0553d7ad20220ca7fbe9
Nikita Nikitin <berozzy@gmail.com>
2013-10-26 00:05:58 +0300

    Working with directories

--- lib/laadur.rb
+++ lib/laadur.rb
@@ -3,12 +3,14 @@ require 'optparse'
 module Laadur
   class CLI
     def initialize
+      sp = Dir.home+'/dev'
+      p Dir.glob sp+"/*" if Dir.exists? sp
       o = OptionParser.new do |o|
         o.banner  = "Available options: "
-        o.on('-o', '--option', String, 'this option does nothing ') do
+        o.on('-o', '--option') do
           p "option:"<<ARGV[0].to_s
         end
-        o.on('-s', '--set', String, 'this option does nothing ') do
+        o.on('-s', '--set') do
           p "set:"<<ARGV[0].to_s
         end
       end
